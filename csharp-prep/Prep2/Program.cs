a56eadeb5dd59f8d39307b5e5b4335bf20673bef
LunchTrey <50647638+treytorrez@users.noreply.github.com>
2025-01-20 19:14:13 -0700

    Finshed code in Prep1 and Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        System.Console.WriteLine("What is your grade? ");
+        int grade = int.Parse(Console.ReadLine());
+        bool isPassing = grade >= 60;
+        string letter = "";
+        switch (grade)
+        {
+            case >= 90:
+                letter = "A";
+                break;
+            case >= 80:
+                letter = "B";
+                break;
+            case >= 70:
+                letter = "C";
+                break;
+            case >= 60:
+                letter = "D";
+                break;
+            default:
+                letter = "F";
+                break;
+        }
+        System.Console.WriteLine($"Your grade is {letter}");
+        System.Console.WriteLine(isPassing ? "You are passing" : "You are failing");
     }
 }
