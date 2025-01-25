070a8a4d3db6ebc6baad518458f116d0d8962d5f
LunchTrey <50647638+treytorrez@users.noreply.github.com>
2025-01-24 22:16:27 -0700

    Reapply "Finished Learning02"
    This reverts commit 366a8e0a874b9ab4ab84a149e13a910996046a7b.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        //Create a new resume
+        Resume resume = new Resume()
+        //Set member variables
+        {_name = "Trey Torrez",
+        _jobs = new List<Job>()
+        //Add jobs inside the resume declaration using an object initializer
+        {
+            new Job()
+            {
+                _company = "Microsoft",
+                _jobTitle = "Software Engineer",
+                _startYear = 2019,
+                _endYear = 2021
+            },
+            new Job()
+            {
+                _company = "Google",
+                _jobTitle = "Software Engineer",
+                _startYear = 2021,
+                _endYear = 2023
+            }
+        }
+        };
+
+        //Display the resume
+        resume.Display();
+
     }
 }
