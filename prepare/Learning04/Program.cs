6c6d7ba3700c0d806a14ad03486b8b47ec72706e
LunchTrey <50647638+treytorrez@users.noreply.github.com>
2025-02-21 23:55:11 -0700

    Completed Learning 04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning04 World!");
+
+        Assignment assignment1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment math1 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(math1.GetSummary());
+        Console.WriteLine(math1.GetHomeworkList());
+
+        WritingAssignment writing1 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writing1.GetSummary());
+        Console.WriteLine(writing1.GetWritingInformation());
     }
 }
